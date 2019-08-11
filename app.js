cf507d14811ff401c290db12c54eedf0ec0ab189
TvNiSp <tvnispdroid@gmail.com>
2019-08-12 01:11:49 +0200

    db config

--- app.js
+++ app.js
@@ -10,15 +10,7 @@ var express = require("express"),
 
 // mongoose.connect('mongodb://localhost:27017/cook_it', {useNewUrlParser: true});
 
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://tvnisp:tornados512@cookit-zwdsp.mongodb.net/test?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
-
+mongoose.connect('mongodb+srv://tvnisp:tornados512@cookit-zwdsp.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true, useCreateIndex: true});
 
 
 // Set the app
