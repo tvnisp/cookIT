38e992b62226c6b783508a683bddfc650ac24934
TvNiSp <tvnispdroid@gmail.com>
2019-08-12 01:00:22 +0200

    corrected online db

--- app.js
+++ app.js
@@ -10,7 +10,15 @@ var express = require("express"),
 
 // mongoose.connect('mongodb://localhost:27017/cook_it', {useNewUrlParser: true});
 
-mongoose.connect('mongodb+srv://tvnisp:tornados512@cookit-zwdsp.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true});
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://tvnisp:tornados512@cookit-zwdsp.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
 
 
 // Set the app
