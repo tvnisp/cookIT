87ec190dbcce014b1ffff68289270bf925595386
TvNiSp <tvnispdroid@gmail.com>
2019-08-12 01:21:38 +0200

    port

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require("express"),
     app     = express(),
-    port    = 3000,
+    port    = 27017,
     landing = require("./routes/landing"),
     recipes = require("./routes/recipes"),
     mongoose = require("mongoose"),
